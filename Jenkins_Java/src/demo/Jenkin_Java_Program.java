0de48840f17edb761589b6cef0b0897afbede44c
QA17 <QA17@QAPLANET-PC>
2020-02-28 12:51:50 +0530

    jenkin java commit

---
+++ Jenkins_Java/src/demo/Jenkin_Java_Program.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.util.ArrayList;
+
+public class Jenkin_Java_Program {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> ar=new ArrayList<String>();
+		ar.add("fisrt nibbi Lucky");
+		ar.add("second nibbi alekhya");
+		ar.add("third nibbi priyanka");
+		
+		System.out.println(ar.get(2));
+		System.out.println(ar.get(1));
+		System.out.println(ar.get(0));
+		
+
+	}
+
+}
