a01ec74eb2d9b9f1df909256801442ad7080ac4a
QA17 <QA17@QAPLANET-PC>
2020-02-28 12:54:19 +0530

    add java println

--- Jenkins_Java/src/demo/Jenkin_Java_Program.java
+++ Jenkins_Java/src/demo/Jenkin_Java_Program.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Jenkin_Java_Program {
 
 	public static void main(String[] args) {
-		
+		System.out.println("first java jenkins program");
 		ArrayList<String> ar=new ArrayList<String>();
 		ar.add("fisrt nibbi Lucky");
 		ar.add("second nibbi alekhya");
